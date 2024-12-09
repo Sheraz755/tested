f94a5ffa39c966c6c0863d217ac57a945af0676f
Sheraz <163497224+Sheraz755@users.noreply.github.com>
2024-12-09 20:45:31 +0500

    tested

---
+++ src/components/header.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { CodeViewer } from "@/components/code-viewer";
+import { PresetSave } from "@/components/preset-save";
+import { PresetSelector } from "@/components/preset-selector";
+import { PresetShare } from "@/components/preset-share";
+
+export function Header() {
+  return (
+    <div></div>
+  );
+}
---
+++ src/components/lk.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import Logo from "@/assets/lk.svg";
+
+export default function LK() {
+  return (
+    <a
+      href="https://callsupport.ai"
+      className="hover:opacity-70 transition-all duration-250"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Call support
+    </a>
+  );
+}
